36b58ca8d119c79e0e6d1560d5500680de1219a0
Igor <guselnikov.uniikm@yandex.ru>
2022-07-10 11:28:49 +0500

    third task

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] array = new int[8];
+arrayFill(array);
+printArray();
+
+void arrayFill(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void printArray ()
+{
+
+    Console.WriteLine($"[{String.Join("-",array)}]");
+}
